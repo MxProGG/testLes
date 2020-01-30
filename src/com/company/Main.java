31a29d78c59dc4fbde89bc91819f51f6a0f1a998
MxProGG <37911573+MxProGG@users.noreply.github.com>
2020-01-30 09:43:06 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,6 +17,7 @@ public class Main {
 
         Scanner inputLesson = new Scanner(System.in);
         System.out.println("Input number lesson: ");
+        System.out.println("Input number lesson: ");
         int numLesson = inputLesson.nextInt();
 
 
